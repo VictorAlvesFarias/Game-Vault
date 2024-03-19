b3d3b6895eae2ddd258629f0a15a2c17b0762605
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2024-03-19 15:45:27 -0300

    Building application

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true  // Add the "node" environment
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -8,8 +12,15 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: '18.2'
+    }
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
@@ -17,4 +28,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
