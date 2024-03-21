1f6a5c0d9b09b76b874dc05679645160ff12e82a
Victor Alves Farias <victor.alves@baraodemaua.br>
2024-03-21 15:18:14 -0300

    Building application

--- src/service/SaveService.ts
+++ src/service/SaveService.ts
@@ -1,18 +1,74 @@
-class SaveItem 
-{
+export class SaveItem {
     constructor(
-        public saveLocal:string,
-        public savePathOrigin:string
-    ) {}
+        public saveLocal: string,
+        public savePathOrigin: string,
+        public saveName: string,
+        public size: string
+    ) { }
 }
 
-export default class SaveService 
-{
-    public async addSave(path) 
-    {
-        const folder = path.split('\\')[path.split('\\').length-1]
-        await application.replaceFolder(path,'./saves/'+folder)
-        await application.writeFileAsync("./saves/save.json","[]")
+export default class SaveService {
+    public async addSave(path) {
+        return (
+            new Promise((resolve, reject) => {
+                try {
+                    let saveContext
+
+                    try {
+                        saveContext = JSON.parse(application.getFile('./saves/save.json', "utf-8") ?? "[]")
+                    }
+
+                    catch (error) {
+                        saveContext = JSON.parse("[]")
+                    }
+
+                    const folder = path.split('\\')[path.split('\\').length - 1]
+                    const saveItem: SaveItem = saveContext?.filter((x: SaveItem) => x?.saveLocal == './saves/' + folder)[0]
+
+                    if (saveItem != null) {
+                        const index = saveContext.indexOf(saveItem)
+                        saveContext[index] = saveItem
+                    }
+
+                    else {
+                        const newSave = new SaveItem(
+                            './saves/' + folder,
+                            path,
+                            folder,
+                            0
+                        )
+                        saveContext.push(newSave)
+                    }
+
+                    application.replaceFolder(path, './saves/' + folder)
+                    application.writeFile('./saves/save.json', JSON.stringify(saveContext))
+                    
+                    return resolve("ok")
+                }
+
+                catch (error) {
+                    return reject("Ocorreu algum erro")
+                }
+            })
+        )
+    }
+
+    public async getSaves() {
+        let saveContext
+
+        try {
+            saveContext = JSON.parse(application.getFile('./saves/save.json', "utf-8") ?? "[]")
+        }
+
+        catch (error) {
+            saveContext = JSON.parse("[]")
+        }
+
+        return (
+            new Promise((resolve, reject) => {
+                return resolve(saveContext)
+            })
+        )
     }
 }
 
