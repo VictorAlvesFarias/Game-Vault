d2c2195612b79be104ad5486ff8addab30fb1f96
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2024-06-12 16:57:39 -0300

    Bug fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,11 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import ButtonDefault from '../styled-components/ButtonDefault';
 import SaveService, { SaveItem } from '../service/SaveService';
+import { Trash } from 'lucide-react';
 
 function Home() {
-
-  const saveService = new SaveService() 
-  const [saves, setSaves] = useState<string[]>([])
+  const saveService = new SaveService()
+  const [saves, setSaves] = useState<any[]>([])
   const formSchema = z.object({
     path: z.string()
   })
@@ -20,43 +20,64 @@ function Home() {
       resolver: zodResolver(formSchema),
     }
   );
-  
-  async function addSave(data) {
-    saveService.addSave(data.path)
+
+  function handleAddSave(data) {
+    saveService.add(data.path).then(e => {
+      handleGetSaves()
+    })
+  }
+
+  function handleDeleteSave(path) {
+    saveService.delete(path).then(e => {
+      setSaves(e)
+    })
+  }
+
+  function handleGetSaves() {
+    saveService.get().then((x: any) => {
+      setSaves(x)
+    })
+  }
+
+  function seelctFile() {
+    window.showDirectoryPicker().then(e=>{
+      console.log(e)
+    })
   }
 
   useEffect(() => {
-    saveService.getSaves().then((x:any)=>{
-      setSaves(x)
-    })
+    handleGetSaves()
   }, [])
-  
+
   return (
     <div className='flex flex-col '>
       <div className='flex flex-col gap-3'>
-        {saves?.map((x:any,i:any)=>
+        <div onClick={handleGetSaves}>
+          <ButtonDefault loading={false}>
+            Verificar arquivos
+          </ButtonDefault>
+        </div>
+        <Form submit={handleSubmit(handleAddSave)}>
+          <InputDefault  register={register("path")} errors={errors.path} label={"Caminho"} />
+          <div onClick={seelctFile}>
+            selecioanr arquivo 
+          </div>
+          <ButtonDefault loading={false}>
+            Adicionar
+          </ButtonDefault>
+        </Form>
+        {saves?.map((x: any, i: any) =>
           <div key={i} className=' p-3 pt-6 rounded bg-zinc-100 flex flex-col relative gap-3'>
-            <div className='cursor-pointer flex justify-end gap-1 top-0 right-0 p-3 absolute'>
-              <div className='w-1 h-1 rounded-full bg-zinc-400'></div>
-              <div className='w-1 h-1 rounded-full bg-zinc-400'></div>
-              <div className='w-1 h-1 rounded-full bg-zinc-400'></div>
-            </div>
             <p>{x.saveLocal}</p>
-            { x.sync?
+            {x.sync ?
               <p className={`bg-green-300 w-fit rounded p-2`}>Status: Sincronizado</p>
               :
               <p className={`bg-red-300 w-fit rounded p-2`}>Status: Não Sincronizado</p>
             }
-
-          </div>  
+            <Trash onClick={() => handleDeleteSave(x.saveLocal)} size={20} />
+          </div>
         )}
       </div>
-      <Form submit={handleSubmit(addSave)}>
-        <InputDefault register={register("path")} errors={errors.path} label={"Caminho"}/>
-        <ButtonDefault loading={false}>
-          Adicionar
-        </ButtonDefault>
-      </Form>      
     </div>
   )
 }
